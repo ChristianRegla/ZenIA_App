e7c434c1d50aad571763804a80d5a834371239c8
ChristianRegla <119276883+ChristianRegla@users.noreply.github.com>
2026-04-07 18:36:01 -0600

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,8 +39,8 @@ android {
         applicationId = "com.zenia.app"
         minSdk = 28
         targetSdk = 36
-        versionCode = 4
-        versionName = "1.0.4"
+        versionCode = 5
+        versionName = "1.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         buildConfigField(
